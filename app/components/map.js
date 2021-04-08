3feb8abeb327410974e97201204857137880d5d9
moraw9 <marzena.orawczak@wp.pl>
2021-04-08 14:14:42 +0200

    displaying data from the server

--- app/components/map.js
+++ app/components/map.js
@@ -6,6 +6,7 @@ const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
 export default class MapComponent extends Component {
     get src(){
         let {lng, lat, width, height, zoom } = this.args;
+
         let coordinates = `${lng},${lat},${zoom}`;
         let dimensions = `${width}x${height}`;
         let accessToken = `access_token=${this.token}`;
