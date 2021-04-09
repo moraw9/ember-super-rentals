79f467097e5bba9fe043562bb941bd62bf70c739
moraw9 <marzena.orawczak@wp.pl>
2021-04-09 09:20:33 +0200

    using Ember Data to manage data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-const COMMUNITY_CATEGORIES = ['Codo', 'Townhouse', 'Apartament']
+
 export default class IndexRoute extends Route {
-    async model(){
-        let response = await fetch('/api/rentals.json');
-        let {data} = await response.json();
-        return data.map((model) => {
-            let {id, attributes } = model;
-            let type;
+    @service store;
 
-            if(COMMUNITY_CATEGORIES.includes(attributes.category))
-                type = 'Community';
-            else
-                type= 'Standalone';
-            
-            return {id, type, ...attributes};
-        });
-    }
+    async model(){
+        return this.store.findAll('rental');
+    };
 }
